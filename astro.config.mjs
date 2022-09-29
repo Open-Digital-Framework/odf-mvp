b12f14190c22e92dfe063c381fab2b6f8e425e61
Neil Dakeyne <neildakeyne@googlemail.com>
2022-09-29 21:47:18 +0100

    remove edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
