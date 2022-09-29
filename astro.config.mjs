ddbec5b9b7243aacea3669319f91fd5337926880
Neil Dakeyne <neildakeyne@googlemail.com>
2022-09-29 21:53:37 +0100

    remove netlify edge functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     // Enable Preact to support Preact JSX components.
-    preact(), // Enable React for the Algolia search component.
+    preact(),
+    // Enable React for the Algolia search component.
     react(),
   ],
-  output: "server",
-  adapter: netlify(),
+  site: `https://odf-mvp.netlify.app`,
 });
